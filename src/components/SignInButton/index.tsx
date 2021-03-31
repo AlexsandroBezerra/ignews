c0cf2eebfdd208e1190593827f7be06bd26aa194
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-03-30 22:24:33 -0300

    feat: save user infos

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -14,8 +14,8 @@ export function SignInButton() {
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#EBA417" onClick={() => signIn('github')} />
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
+      <FaGithub color="#EBA417" />
       Sign in with Github
     </button>
   )
