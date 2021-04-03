39f20e0af1def3150cf8bb3003204cbfc742bb83
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-04-03 13:26:32 -0300

    feat: add post page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+import styles from './styles.module.scss'
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit</strong>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quae libero debitis, optio maxime voluptatum</p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit</strong>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quae libero debitis, optio maxime voluptatum</p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit</strong>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quae libero debitis, optio maxime voluptatum</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
