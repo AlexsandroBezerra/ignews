6f0ef970dd73ae4fac4acb723831c489e7ab0f65
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-04-03 16:51:43 -0300

    feat: validate subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next"
 import Head from "next/head"
-// import { getSession } from "next-auth/client"
+import { getSession } from "next-auth/client"
 import { RichText } from "prismic-dom"
 import { getPrismicClient } from "../../services/prismic"
 
@@ -38,10 +38,17 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  // const session = await getSession()
+  const session = await getSession({ req })
   const { slug } = params
 
-  // if(!session) {}
+  if(!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req)
 
